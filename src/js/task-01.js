a7b7cf8c88822f98787893d92815d14a6d3109ac
JessahSeroje <jseroje.actionlabs@gmail.com>
2023-12-28 02:20:02 +0800

    Task 1 initial answer commit

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,2 +1,7 @@
 const categoriesElements = document.querySelectorAll(".item");
 console.log("Number of categories:", categoriesElements.length);
+
+categoriesElements.forEach((el) => {
+  const categoryName = el.firstElementChild.textContent;
+  console.log("Category:", categoryName);
+});
