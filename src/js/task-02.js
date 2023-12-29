b411bd853f90090a87fe0591a83e39304b1d9069
JessahSeroje <jseroje.actionlabs@gmail.com>
2023-12-29 17:09:05 +0800

    2nd commit

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,30 @@
+// const ingredients = [
+//   "Potatoes",
+//   "Mushrooms",
+//   "Garlic",
+//   "Tomatos",
+//   "Herbs",
+//   "Condiments",
+// ];
+
+// const ingredientsList = documents.querySelector("#ingredients");
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+const ingredientsArray = ingredients.map((element) => {
+  const ingredientItem = document.createElement("li");
+  ingredientItem.classList.add("item");
+  ingredientItem.textContent = element;
+
+  return ingredientItem;
+});
+
+ingredientsList.append(...ingredientsArray);
