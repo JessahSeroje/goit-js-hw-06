2e5fc9299ec100cb1dec19a3c738247779c178ab
JessahSeroje <jseroje.actionlabs@gmail.com>
2023-12-29 16:31:03 +0800

    task-01 commit

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -4,4 +4,8 @@ console.log("Number of categories:", categoriesElements.length);
 categoriesElements.forEach((el) => {
   const categoryName = el.firstElementChild.textContent;
   console.log("Category:", categoryName);
+
+  const categoryTypeList = el.lastElementChild;
+  const categoryTypesLength = categoryTypeList.children.length;
+  console.log("Elements:", categoryTypesLength);
 });
